e7b3683cda994caf0fc14cd3e61ef01d2fb3e1ad
chaoukat99 <chawkatomar@gmail.com>
2026-05-22 20:43:58 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,722 @@
+
+// let btn= document.querySelector("button")
+// btn.onclick=()=>{
+//  alert("hello world")
+
+// }
+
+
+// /*
+
+
+// basics : 
+
+// var & contants
+// datatypes 
+// operators 3
+// conditions 
+// les boucles
+// -alert prompt and confirm
+// arrays 
+// objects 
+// function vs procedure 
+// spread & rest operators
+// exceptions
+
+// -ES6
+
+// -Higher Order Functions
+// -JSON
+
+// -DOM (document object model )
+
+// -BOM (browser object model )
+
+// -API (fetch ) promise
+
+// // intos js 
+// // chart js 
+// // loadash 
+// // wow js 
+// // gsap 
+
+
+
+// // REACT 
+
+// // NODE js 
+// // mongodb 
+// // express 
+
+
+
+
+
+
+// // vari
+
+// */
+
+
+
+
+
+// // variable & constante 
+
+// /*
+
+
+// let username= "youness" 
+
+//  username="mouad"
+//  username="ahmed"
+// let age=20
+// console.log(username)
+// console.log("-".repeat(100))
+
+// console.log(username)
+
+// console.log(age
+
+
+// console.log(password)
+
+// */
+// // document.querySelectorAll("h1").forEach(el=>el.innerHTML=username)
+
+
+
+
+
+// const  PI= 3.14159265359
+
+// //PI= 3.15
+
+// const username = "karim";
+
+// //username=100
+
+// console.log(PI) 
+// console.log(username)
+
+
+
+
+// // data types
+
+// // youness casablanca 2010 false 15000.50 null undefined
+
+
+// // primtive data types
+
+
+// // string text "text".  'text'.  `text`  
+ 
+// let user1="2000"
+// let user2='youness'
+// let user3=`youness`
+
+// console.log(user1)
+// console.log(user2)
+// console.log(user3)
+
+// // number 
+// let stock=20
+
+
+
+// let note=5.75
+
+
+// // boolean
+
+// let rain = true 
+// let sun= false
+
+// let operation = 10<11
+// console.log(operation)
+
+
+// let user=null
+
+
+
+
+// // STrings "hello" 't' "hdjsh176Z76" 
+// // Numbers 10 14.12 2000 
+
+
+
+// //let salary=1e5;
+// // boolean true or false 
+// let weadtherSunny=true;
+
+
+// // null 
+// let users = null ;
+
+
+
+// // student 
+// let studentName = "yassin"
+// let studentAge = 20
+// let studentNote = 15.5
+// let studentIsMarried = false
+// let studentCity = null
+
+// // school
+// let schoolName = "mohamed 5"
+// let schoolAge = 25
+// let schoolNote = 17.5
+// let schoolIsMarried = false
+// let schoolCity = null 
+
+
+
+
+// // mot de passe string number 
+
+
+
+
+
+// // operateur 
+
+// // Arithmetique  + - * / % 
+// let num1=10;
+// let num2=4
+
+// console.log(num1+num2)
+// console.log(num1-num2)
+// console.log(num1*num2)
+// console.log(num1/num2)
+// console.log(num1%num2)
+
+
+// // comparaison 
+
+// //. < > <= >= == === != !==
+
+// // age=20;
+
+// // console.log(age > 18) // true
+// // console.log(age < 18) // false
+// // console.log(age <= 18) // false
+// // console.log(age >= 18)
+// // console.log(age == 18)  // false 
+
+// // console.log(age!=18) 
+
+
+
+
+
+// // let age ="20"
+   
+// // console.log(age==20) // true 
+
+// // console.log(age==15) //false 
+
+
+// // let backgroundColor="red" // camelCase
+
+
+
+
+// // let password ="1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+
+
+// // console.log(password.length)
+
+
+
+
+// // LOGIQUE &&(and)    ||(or)  !(not)
+
+
+
+// let age=20;
+// let gender="male"
+
+// console.log(age>=18 && gender=="male")
+
+
+// // true     && true    -> true
+// // true     && false   -> false
+// // false    && true    -> false
+// // false    && false   -> false
+
+// console.log(age>18 || gender=="male")
+
+// // true     || true    -> true
+// // true     || false   -> true
+// // false    || true    -> true
+// // false    || false   -> false
+
+
+
+
+// // let a=10;
+// // let b=5;
+// // let c=a+b
+// // let d=c+5;
+// // let e=d+a;
+
+
+
+
+
+// // console.log( ( (c / b) === 3 && (e - d) >= a ) || "JavaScript".length < 5 );
+
+
+
+// // concatenation   
+// // let name="khalid";
+
+
+// // console.log("hello my name"+"\n"+name)
+
+
+// // let name,age,profession , address , phone 
+
+
+// // FullName:Karim shgdshgd
+// // Age:20
+// // Address:123 Main St
+// // Phone:123456789
+// // Profession:Soft
+// // ware Engineer
+
+
+
+
+// //  alert prompt confirm
+
+
+
+// // const num3=+prompt("num1 : ")
+
+// // const num4=+prompt("num2: ")
+
+
+// // alert("La somme des deux nombres est "+ (num3+num4))
+
+
+
+
+
+// // const condition=confirm("are you sure ? ")
+
+// // alert(condition)
+
+
+
+// // conditions (chorout )
+
+// // if(condition){. condition 10>20 "omar"== "Omar"
+// //     // traitement 
+// // }
+
+
+
+
+// const userAge=20;
+
+
+// // if(userAge>18){
+// //     console.log("Adulte")
+// // }
+
+// // if(userAge>21){
+// //     console.log("Adulte pro max")
+
+// // }
+
+
+
+// // if(userAge>18){
+// //     console.log("Adulte")
+// // }else{
+// //     console.log("Mineur")
+// // }
+
+
+
+
+
+// // if imbriqué 
+
+
+
+
+
+
+
+// let day=5;
+
+
+// // if(day==1){
+// //     console.log("lundi")
+// // }else if(day==2){
+// //     console.log("mardi");
+    
+// // }else if(day==3){
+// //     console.log("mercredi")
+// // }else if(day==4){
+// //     console.log("jeudi")
+// // }else if(day==5){
+// //     console.log("vendredi")
+// // }else if(day==6){
+// //     console.log("Samedi")
+// // }else if(day==7){
+// //     console.log("Dimanche")
+// // }else{
+// //     console.log("Invalid day");
+    
+// // }
+
+
+
+
+// // switch case 
+
+
+
+
+// switch(day){
+//     case 1:{
+//         console.log("Lundi")
+//     }break;
+//     case 2:{
+//         console.log("Mardi")
+//     }break;
+//     case 3:{
+//         console.log("Mercredi")
+//     }break;
+//     case 4:{
+//         console.log("Jeudi")
+//     }break;
+//     case 5:{
+//         console.log("Vendredi")
+//     }break;
+//     case 6:{
+//         console.log("Samedi")
+//     }break;
+//     case 7:{
+//         console.log("Dimanche")
+//     }break;
+//     default:{
+//         console.log("invalid day")
+//     }
+// }
+
+
+// POST GET 
+// /product
+
+
+// switch(request.method){
+//     case "GET":{
+//         //
+//     }break;
+//     case "POST":
+// }
+
+
+
+
+    // user insére deux nombres num1 et num2 
+    // user insére operateur (+ - x / )
+    // resultat doit etre affiche basé sur l'opérateur choisi
+
+
+
+
+
+    // user enter password 
+    //  .length
+
+
+    // password weak  sghar 6
+    // password medium mabin 7 u 11
+    // password strong kbr mn 15
+
+    // alert password (weak medium strong)
+
+
+
+    // ternary condition 
+
+
+    // age=21;
+
+
+// let Status=age>18?"Adulte":"Mineur";
+// consition ? true:false
+
+// console.log(age>18?"Adulte":"Mineur")
+
+// console.log(Status)
+
+     
+// Loops (boucles)
+
+
+
+// for Loop 
+// while loop
+// do while 
+// Use case
+
+
+
+
+// for loop (developer nb de repetition )
+    
+
+// for(let i=0;i<5;i++){
+//     console.log("hello " + i)
+// }
+
+// i=0 ; true ; i+1=0+1=1 i=1
+// i=1 ; true ; i+1 =1+1=2
+// i=2 ; true ; i+1 =2+1=3
+// i=3 ; true ; i+1 = 3+1=4
+// i=4; true ; i+1= 4+1 =5
+// i=5 ; false
+
+
+
+// while(condition)
+
+// while(confirm("Do you to repeat saying Hello ?")){
+//     // traitement a répeter
+//     alert("Hello world")
+// }
+
+
+
+// console.log("ela slamtek")
+
+
+
+
+// -afficher un prompt et demander un nb qui se devise paR 5
+// - si le nb se divise sur 5 on sort de la boucle et on affichant un message "tbarkelah elik"
+// - sinon la boucle se repete des le debut 
+
+
+
+
+// opearteur  > < == >= <=  2!=5
+
+// arith. + - /   2%3 
+
+
+
+// nb % 5 != 0
+
+// while
+
+
+// do while 
+// let test=false;
+//  do{
+//     console.log("hello this is from do while loop")
+//  }while(test)
+// while(test){
+//     console.log("hello this is from do while loop")
+
+// }
+
+
+
+// let str="hello world ";
+// break
+// continue
+
+
+
+
+
+// for(let i=0;i<1000;i++){
+
+//     if(i==500){
+//         break;
+//     }
+    
+//     console.log("hello we are seraching for user number 500 " + i);
+    
+// }
+
+
+// for(let i=0;i<1000;i++){
+
+//     if(i==500 || i==10){
+//       continue
+//     }
+    
+//     console.log("hello we are exluding  user number 500 " + i);
+    
+// }
+
+
+
+
+
+
+
+// string (text)
+
+// NUmber. 10.5 , 20.9 , 2000 ,10/5 , 20+30
+
+// boolean  true false
+
+
+// null
+
+
+
+// let currenrUser=null;
+// currenrUser="Hamid";
+
+
+// let userName="karim"
+
+// userName="samir";
+
+// console.log(userName[userName.length-1])
+// //console.log(userName.length)
+
+// console.log(20/3)
+
+
+// console.log(1e9)
+
+
+
+
+
+
+
+// for while do  while 
+
+
+
+
+
+
+// opearteurs 
+
+// +    / -  *  ARithmetique
+
+
+// let a=12;
+// let b= "12"; 
+
+
+// console.log(a!==b)
+// console.log( (+a) + (+b) );
+
+// console.log( +a / 0 )
+
+
+// < > <= >= == === !=
+
+
+
+
+
+// Logique 
+
+// and &&  or ||  not !
+
+
+// let age=20;
+// let pass ="hello123"
+// let userExist=true;
+
+// if(!userExist){
+//     console.log("user hors ligne")
+// }else{
+//     console.log("User connected")
+// }
+
+
+
+
+
+let userEmail = "karim@gmail.com"
+let user2Email = "Samir@gmail.com"
+
+
+// Js & Py List
+// Tableau 
+let users = ["karim@gmail.com","samir@gmail.com","brahim@gmail.com","ismail@gmail.com","said@gmaiçl.com"]
+
+
+// Reading Items 
+
+// console.log(users[0])
+// console.log(users[1])
+
+// console.log(users[2])
+
+// console.log(users[3])
+
+// console.log(users[4])
+
+
+
+// for(let i=0;i<users.length;i++){
+//     console.log(users[i]);
+    
+// }
+// for(let i in users){
+//     console.log(users[i])
+// }
+
+// for(let i of users){
+//     console.log("Bonjour utilisatuer qui a l'eamil "+i)
+// }
+
+
+
+//  console.log(users[0]);
+//  console.log(users[1]);
+
+
+let skills=["Html","css"];
+
+console.log(skills);
+
+
+// skills[skills.length]="Js";
+
+skills[0]="Html for web"
+console.log(skills);
+
+
+
+
+
+
+let prices = [300,250,430,1001,2900,79,450,321,981];
+
+
+const paire=[] ;
+const Impair= []; 
+
+console.log("Paire")
+
+console.log(paire)
+
+console.log("ImPaire")
+
+console.log(Impair)
+
+
+
+
+let names=["samir","omar","samir","karim","hamid","samir"]
